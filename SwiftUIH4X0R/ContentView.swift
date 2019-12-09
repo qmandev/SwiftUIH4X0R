f005db74a81f95c33e2263b718ef7246d48866d0
Qiang Ma <qiangma7@gmail.com>
2019-12-09 11:55:01 -0500

    Initial Commit

---
+++ SwiftUIH4X0R/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUIH4X0R
+//
+//  Created by ARMSTRONG on 12/9/19.
+//  Copyright © 2019 ARMSTRONG. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
